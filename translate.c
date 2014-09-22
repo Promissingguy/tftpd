dca81f804ce73ddb987027d7e5761da6a94b5429
renwei <527248603@qq.com>
2014-09-22 06:52:21 -0700

    add all

---
+++ translate.c
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include "translate.h"
+	
+int myrecvdata(int sock, Remote_information * remote)
+{
+	
+	int ret ;
+	int len ;
+	len = sizeof(remote->remoteaddr);
+//	memset(remote->buf,0,sizeof(remote->buf));
+//	memset(&(remote->remoteaddr) ,0 ,sizeof(remote ->remoteaddr));
+	memset(remote ,0 ,sizeof(Remote_information));
+	ret = recvfrom(sock ,remote->buf,BUFLEN,0,&(remote->remoteaddr),&len);
+	return ret ;
+	
+}
+
+int mysenddata(int sock ,Remote_information* remote,int buflen)
+{
+	int ret  ;
+	ret = sendto(sock ,remote->buf,buflen,0,&(remote->remoteaddr),sizeof(remote->remoteaddr));
+	return ret ;
+}
